1ff754ed9bbfa9ba5eb300b5117fd90ca6f79e21
Irenewsh <Irene.wsh1995@gmail.com>
2019-07-15 22:41:28 -0400

    JS basic

---
+++ Module6_JS_Basic/JSbasic.js
@@ -0,0 +1,8 @@
+var firstname = prompt("what is your firstname");
+var lastname = prompt("what is your lastname");
+var age = prompt("what is your age");
+
+console.log("You're "+ firstname + " " + lastname + age + " years old.");
+
+var days = age*365;
+alert(days);
