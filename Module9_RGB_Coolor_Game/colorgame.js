a3f25a1eff828992330059b1fa274da505cf551a
Irenewsh <Irene.wsh1995@gmail.com>
2019-07-18 23:52:18 -0400

    RGB Color Game advanced

--- Module9_RGB_Coolor_Game/colorgame.js
+++ Module9_RGB_Coolor_Game/colorgame.js
@@ -1,11 +1,61 @@
 var colors = generateColor(6);
 
 var boxes = document.querySelectorAll(".box");
-var pickcolor = pickColor();
+var pickedcolor = pickColor();
 var colordisplay = document.getElementById("colordisplay");
 var messagedisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var reset = document.querySelector("#reset");
+var easy  = document.querySelector("#easy");
+var hard = document.querySelector("#hard");
 
-colordisplay.textContent = pickcolor;
+easy.addEventListener("click", function(){
+	hard.classList.remove("selected");
+	easy.classList.add("selected");
+	colors = generateColor(3);
+	pickedcolor = pickColor();
+	colordisplay.textContent = pickedcolor;
+	for(var i = 0; i < boxes.length; i++){
+		if (colors[i]){
+		boxes[i].style.backgroundColor = colors[i];}
+		else{
+		boxes[i].style.display = "none"; //hide no need ones
+		}
+	h1.style.backgroundColor = "#232323";
+	}
+});
+	
+
+hard.addEventListener("click",function(){
+	easy.classList.remove("selected");
+	hard.classList.add("selected");
+	colors = generateColor(6);
+	pickedcolor = pickColor();
+	colordisplay.textContent = pickedcolor;
+	for(var i = 0; i < boxes.length; i++){		
+		boxes[i].style.backgroundColor = colors[i];		
+		boxes[i].style.display = "block"; //hide no need ones
+		}
+	h1.style.backgroundColor = "#232323";
+	}
+
+);
+
+reset.addEventListener("click", function(){
+	// generate new colors
+	colors = generateColor(6);
+	// pick the target color
+	pickedcolor = pickColor();
+	// change color displayed to match picked one
+	colordisplay.textContent = pickedcolor;
+	// update boxes color
+	for(var i = 0; i < boxes.length; i++){
+		boxes[i].style.backgroundColor = colors[i];
+	}
+	h1.style.backgroundColor = "#232323";
+})
+
+colordisplay.textContent = pickedcolor;
 
 for (var i = 0; i < boxes.length; i++){
 	//add initial colors to boxes
@@ -15,9 +65,10 @@ for (var i = 0; i < boxes.length; i++){
 	boxes[i].addEventListener("click", function(){
 		//grab color from selected box
 		var clickedcolor = this.style.backgroundColor;
-		if (clickedcolor === pickcolor){
+		if (clickedcolor === pickedcolor){
 		messagedisplay.textContent = "you win!";
-		changecolor(pickcolor);
+		changecolor(pickedcolor);
+		h1.style.backgroundColor = clickedcolor;
 		}
 		else{
 			this.style.backgroundColor = "#232323";
@@ -52,10 +103,70 @@ function randomColor(){
 	var r = Math.floor(Math.random() * 256);
 	var g = Math.floor(Math.random() * 256);
 	var b = Math.floor(Math.random() * 256);
-	return "rgb(" + r + "," + g + "," + b + ")"; //make up "rgb(#,#,#)"
+	return "rgb(" + r + ", " + g + ", " + b + ")"; //make up "rgb(#,#,#)"
 
 }
+// ***************************** version 3 *******************************
+// generate random color, but no "new color" buttons.etc
+// var colors = generateColor(6);
 
+// var boxes = document.querySelectorAll(".box");
+// var pickcolor = pickColor();
+// var colordisplay = document.getElementById("colordisplay");
+// var messagedisplay = document.querySelector("#message");
+// var h1 = document.querySelector("h1");
+
+// colordisplay.textContent = pickcolor;
+
+// for (var i = 0; i < boxes.length; i++){
+// 	//add initial colors to boxes
+// 	boxes[i].style.backgroundColor = colors[i] //loop through all colors
+
+// 	//click boxes
+// 	boxes[i].addEventListener("click", function(){
+// 		//grab color from selected box
+// 		var clickedcolor = this.style.backgroundColor;
+// 		if (clickedcolor === pickcolor){
+// 		messagedisplay.textContent = "you win!";
+// 		changecolor(pickcolor);
+// 		h1.style.backgroundColor = clickedcolor;
+// 		}
+// 		else{
+// 			this.style.backgroundColor = "#232323";
+// 			messagedisplay.textContent = "Try again!";
+// 		}
+// 	});
+// }
+
+// function changecolor(color){
+// 	// loop through all boxes
+// 	for (var i = 0; i < boxes.length; i++){
+// 		boxes[i].style.backgroundColor = color;
+// 	}
+// 	//
+// }
+
+// function pickColor(){
+// 	var random = Math.floor(Math.random() * colors.length)
+// 	return colors[random];
+// }
+
+// function generateColor(num){
+// 	var arr = [];
+// 	for (var i = 0; i < num; i++){
+// 		arr.push(randomColor());
+// 	}
+// 	return arr;
+// }
+
+// function randomColor(){
+// 	//pick a red from 0-255, grenn from 0-255 and a blue
+// 	var r = Math.floor(Math.random() * 256);
+// 	var g = Math.floor(Math.random() * 256);
+// 	var b = Math.floor(Math.random() * 256);
+// 	return "rgb(" + r + ", " + g + ", " + b + ")"; //make up "rgb(#,#,#)"
+
+// }
 // ***************************** basic version 2 *****************************
 // determine if there correct color was picked, change relative places into correct color if correct
 // var colors = [
