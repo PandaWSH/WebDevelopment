9b94ae85e6aff4da57510250b2a78b804dfdd763
Irenewsh <Irene.wsh1995@gmail.com>
2019-08-08 23:29:25 -0400

    Style finished

--- YelpProject/app.js
+++ YelpProject/app.js
@@ -6,9 +6,16 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 
-var campgrounds = [
+var foods = [
 		{name: "Salmon", image:"https://natashaskitchen.com/wp-content/uploads/2018/05/Pan-Seared-Salmon-with-Lemon-Butter-Sauce-5.jpg"},
 		{name:"Dumplings",image:"https://www.thespruceeats.com/thmb/aMte6tsT-uBbnCvQZi2XtjwAemU=/4000x3000/smart/filters:no_upscale()/korean-dumpling-mandoo-2118676_hero-01-779525b88e834969b81f9a824bc0bf9b.jpg"},
+		{name:"Rice noodles", image:"https://www.halfbakedharvest.com/wp-content/uploads/2018/08/Saucy-Coconut-Summer-Curry-with-Rice-Noodles-and-Garden-Vegetables-1.jpg"},
+		{name:"Instagram post 08-08-2019", image:"https://scontent-lga3-1.cdninstagram.com/vp/2ced365725ba76743814230a8f3667f1/5DEC5A7E/t51.2885-15/e35/s1080x1080/66650312_185456442471099_8203707413253605632_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com"},
+		{name:"Instagram post 08-0702019", image:"https://scontent-lga3-1.cdninstagram.com/vp/78bb17d8a43a427b14b0b4c91026402d/5DCB9628/t51.2885-15/e35/s1080x1080/68898609_445498566047701_5997042091357307574_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com"},
+	 	{name:"Instagram post xx-xx-xxxx", image:"https://scontent-lga3-1.cdninstagram.com/vp/cd42810f0e92044209e3b6686b33b6a3/5DEC9478/t51.2885-15/e35/s1080x1080/66270062_470478073787308_1887982875598276185_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com"},
+	{name:"Instagram post 08-08-2019", image:"https://scontent-lga3-1.cdninstagram.com/vp/2ced365725ba76743814230a8f3667f1/5DEC5A7E/t51.2885-15/e35/s1080x1080/66650312_185456442471099_8203707413253605632_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com"},
+		{name:"Instagram post 08-0702019", image:"https://scontent-lga3-1.cdninstagram.com/vp/78bb17d8a43a427b14b0b4c91026402d/5DCB9628/t51.2885-15/e35/s1080x1080/68898609_445498566047701_5997042091357307574_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com"},
+	{name:"Dumplings",image:"https://www.thespruceeats.com/thmb/aMte6tsT-uBbnCvQZi2XtjwAemU=/4000x3000/smart/filters:no_upscale()/korean-dumpling-mandoo-2118676_hero-01-779525b88e834969b81f9a824bc0bf9b.jpg"},
 		{name:"Rice noodles", image:"https://www.halfbakedharvest.com/wp-content/uploads/2018/08/Saucy-Coconut-Summer-Curry-with-Rice-Noodles-and-Garden-Vegetables-1.jpg"}
 	]
 	
@@ -16,24 +23,24 @@ app.get("/", (req, res) => {
 	res.render("landing"); //landing page, ejs file
 });
 // *******************************Group 1*********************************
-app.get("/campgrounds", (req, res) => {
-	//temporally moved the var campgrounds to the top as a universal variable	
-	res.render("campgrounds",{campgrounds:campgrounds});
+app.get("/foods", (req, res) => {
+	//temporally moved the var foods to the top as a universal variable	
+	res.render("foods",{foods:foods});
 });
 
-app.post("/campgrounds", (req, res) => {
-	// get data from form and add to campgrounds array	
+app.post("/foods", (req, res) => {
+	// get data from form and add to foods array	
 	var name = req.body.name;
 	var image = req.body.image;
-	var newCampground = {name: name, image: image}; //the object to be pushed.
-	campgrounds.push(newCampground); //it has to be an object to be pushed.
-	res.redirect("/campgrounds"); //default as get request
-	//redirect to the campground page
+	var newFood = {name: name, image: image}; //the object to be pushed.
+	foods.push(newFood); //it has to be an object to be pushed.
+	res.redirect("/foods"); //default as get request
+	//redirect to the food page
 });
 // *******************************Group 1*********************************
 
 // *******************************Group 2*********************************
-app.get("/campgrounds/new", (req, res)=> {
+app.get("/foods/new", (req, res)=> {
 	res.render("new.ejs");	
 });
 // *******************************Group 1*********************************
