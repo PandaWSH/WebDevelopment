28d207c3a2d1fba47dff6e0af7ad69da784b7cf1
Irenewsh <Irene.wsh1995@gmail.com>
2019-08-11 22:40:27 -0400

    done

--- RESTful_Blog/app.js
+++ RESTful_Blog/app.js
@@ -1,12 +1,15 @@
 var express = require("express"),
 	bodyParser = require("body-parser"),
 	mongoose = require("mongoose"),
+	methodOverride = require("method-override"),
 	app = express();
+
 
 mongoose.connect("mongodb://localhost/restful_blog",{useNewUrlParser:true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static("public"));
+app.use(methodOverride("_method"));
 
 var blogSchema = new mongoose.Schema({
 	title: String,
@@ -59,6 +62,40 @@ app.get("/blogs/:id", (req, res) =>{
 	});
 }); 
 
+// EDIT ROUTE
+app.get("/blogs/:id/edit", (req, res) =>{
+	Blog.findById(req.params.id, (err, foundBlog) =>{
+		if(err){
+			res.redirect("blogs");
+		} else{
+			res.render("edit", {blog: foundBlog});
+		}
+	});
+});
+
+// UPDATE ROUTE (after editing, it needs to be updated)
+app.put("/blogs/:id", (req, res) => {
+	Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog) => {
+		if(err){
+			res.send("There's error");
+		} else {
+			res.redirect("/blogs/" + req.params.id);
+		}
+	});
+});
+
+// DELETE ROUTE
+app.delete("/blogs/:id", (req, res) => {
+	Blog.findByIdAndRemove(req.params.id, (err) => {
+	 if(err){
+		 res.redirect("/blogs");
+	 } else {
+		 res.redirect("/blogs");
+	 }
+	});
+});
+
+// ****************************************** set up ***************************************
 app.listen(9000, () => {
 	console.log("server test ok!");
 });
