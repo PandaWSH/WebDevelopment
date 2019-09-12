f3e2067ff9d785c845d41ca4a68c6709bb632672
你好呀Panda <Irenewsh@10-18-115-175.dynapool.wireless.nyu.edu>
2019-09-11 20:07:04 -0400

    finished skeleton, working on hoem-page jQuery

---
+++ Speed Reader App/index.js
@@ -0,0 +1,28 @@
+$(function() {
+	var textArray;
+	var inputLength;
+
+	$("#new").hide();
+	$("#resume").hide();
+	$("#pause").hide();
+	$("#controls").hide();
+	$("#result").hide();
+	$("#error").hide();
+
+	// click on "start" to start reading
+	$('start').click(function(){
+		//get text and split into array
+		textArray = $("#userInput").val().split(/\s+/); // \s matches space, line, etc
+		inputLength = textArray.length();
+
+		//conditions
+		if(inputLength>1){
+
+		}else{
+			$('error') //show error message because no enough input content
+		}
+	});
+
+
+
+});
